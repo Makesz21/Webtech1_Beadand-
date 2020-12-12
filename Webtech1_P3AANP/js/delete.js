573c8c053ee10345716cc6e0f2fd8a4db515d323
Makesz21 <65510279+Makesz21@users.noreply.github.com>
2020-12-12 22:15:32 +0100

    Add files via upload

---
+++ Webtech1_P3AANP/js/delete.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    var carsForm = $("#delcars");
+    var manufacturersForm = $("#delmanufacturers");
+
+    carsForm.submit(function(e) {
+
+        e.preventDefault();
+        console.log(e);
+        console.log(this);
+        var json = $(this).serializeArray();
+        console.log(json[0].value);
+
+        var obj = {
+            _id: json[0].value,
+        };
+
+        console.log(obj);
+
+        $.ajax({
+                type: "DELETE",
+                url: "https://webtechcars.herokuapp.com/api/cars/" + json[0].value,
+                data: JSON.stringify(obj),
+                contentType: "application/json",
+            })
+
+            .done(console.log)
+            .fail(console.log);
+    });
+
+    manufacturersForm.submit(function(e) {
+
+        e.preventDefault();
+        var json = $(this).serializeArray();
+        console.log(json[0].value);
+
+        var obj = {
+            _id: json[0].value,
+        };
+
+        console.log(obj);
+
+        $.ajax({
+                type: "DELETE",
+                url: "https://webtechcars.herokuapp.com/api/manufacturers/" + json[0].value,
+                data: JSON.stringify(obj),
+                contentType: "application/json",
+            })
+
+            .done(console.log)
+            .fail(console.log);
+    });
+
+});
